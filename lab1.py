8f1a6202b76031c8be1db554d7594f97dd19dbd6
Anthony Gimei <ag@Anthonys-MacBook-Pro.local>
2018-04-14 09:55:14 -0400

    lab 1

---
+++ lab1.py
@@ -0,0 +1,29 @@
+'''
+We are baking brownies!  We have a recipe which calls for the following ingredients:
+0.5 cups butter
+2 eggs
+1 teaspoon vanilla
+1 cup sugar
+0.5 cups flour
+0.5 cups cocoa powder
+0.25 teaspoons baking powder
+0.25 teaspoons salt
+The ingredients in this recipe will make 9 brownies.
+Write a program which asks the user how many brownies they wish to bake,
+then displays the adjusted recipe with the correct amounts for making that number of brownies.
+Don't worry about converting teaspoons to cups (etc) - just display the amounts in their original measurements.
+'''
+
+number_brownies = int(input("Enter number of Brownies to make: "))
+cooking_ratio = float(number_brownies/9)
+#Now print the recipe
+print(0.5*cooking_ratio,"cups butter")
+print(2*cooking_ratio,"eggs")
+print(1*cooking_ratio,"teaspoon vanilla")
+print(1*cooking_ratio,"cup sugar")
+print(0.5*cooking_ratio,"cups flour")
+print(0.5*cooking_ratio,"cups cocoa powder")
+print(0.25*cooking_ratio,"teaspoons baking powder")
+print(0.25*cooking_ratio,"teaspoons salt")
+
+
