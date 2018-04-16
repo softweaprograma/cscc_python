f242a89cc29afee61639d0fc48aa820ed09937e6
Anthony G <35231092+softweaprograma@users.noreply.github.com>
2018-04-16 08:53:34 -0400

    Create converter_feet_inches_meters.py
     Lesson 1 Activities Challenge: 
    Convert feet and inches to meters and centimeters.

---
+++ converter_feet_inches_meters.py
@@ -0,0 +1,28 @@
+# FILE: converter_feet_inches_meters.py
+# NAME: Length Converter
+# AUTHOR: Anthony Gimei
+# DATE: 4/16/2018
+# PURPOSE: Converts Feet and Inches to Meters and Centimeters
+
+while True:
+    try:
+        input_feet = float(input("enter feet: "))
+        break
+    except ValueError:
+        print("Please enter Numbers only... try again")
+
+while True:
+    try:
+        input_inches = float(input("enter inches: "))
+        break
+    except ValueError:
+        print("Please enter Numbers only... try again")
+        
+output_centimeters = input_feet * 30.48
+output_centimeters = output_centimeters + (input_inches * 2.54)
+output_meters = output_centimeters/100
+
+print()
+print(output_meters," m")
+    
+            
