d206956469f5a7bb1f60014b02ad6877df052f0f
gimeia1 <anthony.gimei@nationwide.com>
2018-04-25 11:31:09 -0400

    made updates

--- the_guessing_game.py
+++ the_guessing_game.py
@@ -2,35 +2,25 @@
 # NAME: Displays if user guessed correct number
 # AUTHOR: Anthony Gimei
 # DATE: 4/24/2018
-# PURPOSE: Accepts guest user input and lets them know if they guessed correctly
-# Submission confirmation number:
+# PURPOSE: Accepts user guess then lets them know if they guessed correctly
 
 import random
 random_low = 1
 random_high = 5
-guess_is_good = False
-continue_playing = False
+continue_playing = True
 
-while True:
+while continue_playing:
     random_number = random.randint(random_low, random_high)
-    print(random_number)
     try:
         user_guess = float(input("Guess a number between 1 and 5:\n"))
-        for x in range(random_low, random_high):
-            if round(user_guess) == random_number:
-                print("You won! Number", round(user_guess), "is the winner")
-                guess_is_good = True
-                break
-            else:
-                print("Try your luck again.")
-                break
-        if guess_is_good:
-            want_to_continue = input("Do you want to play again?\n")
-            if want_to_continue.lower() != "y" or want_to_continue != "yes":
+        if round(user_guess) == random_number:
+            print("You won! Number", round(user_guess), "is the winner")
+            want_to_continue = input("Do you want to play again(Type 'y' or 'yes' to play again)?\n")
+            if want_to_continue.lower() == "y" or want_to_continue == "yes":
                 continue_playing = True
             else:
-                break
-        if not continue_playing:
-            break
+                continue_playing = False
+        else:
+            print("You've guessed the wrong number.\nTry your luck again.\n")
     except ValueError:
         print("Please Enter Valid Number only... try again")
