c8faee6c501e12fcf6aead55c16c80f3ff663070
gimeia1 <anthony.gimei@nationwide.com>
2018-05-06 21:46:58 -0400

    updates

--- encoder_decoder.py
+++ encoder_decoder.py
@@ -1,12 +1,13 @@
 # FILE: encoder_decoder.py
 # NAME: Encodes Or Decodes user input
 # AUTHOR: Anthony Gimei
-# DATE: 5/3/2018
+# DATE: 5/6/2018
 # PURPOSE: Encodes or Decodes input string from user using certain rules
 # Submission confirmation number:
 
 # encoding A=1,B=2,C=3,D=4,E=5,F=6,G=7,H=8,I=9,J=10,K=11,L=12,M=13,N=14,O=15,P=16,Q=17,R=18,S=19,T=20,U=21,V=22,W=23,X=24,Y=25,Z=26
 alpha = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P","Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+alphabet = "abcdefghijklmnopqrstuvwxyz"
 
 # Define a function for decoding
 def encode(string_to_encode):
@@ -49,6 +50,8 @@ def decode(string_to_decode):
             word_to_decode_array = word_to_decode.split("-")
             decoded_word = ""
             for number in word_to_decode_array:
+                if len(number) > 2:
+                    number = number[:2]
                 # print(number)
                 # decode letter and put it in new string
                 counter = 0
@@ -59,28 +62,43 @@ def decode(string_to_decode):
                             #print(letter)
                             decoded_word = decoded_word + letter
 
-
             decoded_words_array[i] = decoded_word
 
     decoded_string = ""
     for word in decoded_words_array:
         decoded_string = decoded_string + word + " "
 
-    return decoded_string
+    return decoded_string.capitalize()
 
 
 def decode_v2(string_to_decode):
     number_to_decode = ""
+    decoded_word = ""
+    counter = 0
     for char in string_to_decode:
+        counter += 1
         if str(char).isnumeric():
             number_to_decode += char
-        elif str(char) == "-":
-            number_to_decode = number_to_decode
+        elif str(char) == "-" or str(char) == " ":
+            # print(number_to_decode)
+            # if not str(char) == "-":
+            decoded_word += number_to_decode + char
+            number_to_decode = ""
+        else:
+            # print(number_to_decode)
+            decoded_word += number_to_decode + char
+            number_to_decode = ""
+        if str(char) == ' ':
+            print(decoded_word)
+            decoded_word = ""
+        if counter  == len(string_to_decode):
+            print(decoded_word)
+            decoded_word = ""
 
+    # return number_to_decode
 
 
 def encode_v2(string_to_encode):
-    alphabet = "abcdefghijklmnopqrstuvwxyz"
     string_to_encode = string_to_encode.lower()
     str_len = len(string_to_encode)
     decoded_str = ""
@@ -91,10 +109,18 @@ def encode_v2(string_to_encode):
         else:
             decoded_str += x
 
-
     return decoded_str
 
 
-encoded_str = encode_v2("How are you?")
-print(encoded_str)
-#print(decode(encoded_str))
+while True:
+    try:
+        user_input = (input("Enter String to Encode or decode\n"))
+        if user_input[0].isnumeric():
+            print(decode(user_input))
+            break
+        else:
+            print(encode(user_input))
+            break
+        break
+    except ValueError:
+        print("Please enter Valid string... try again")
